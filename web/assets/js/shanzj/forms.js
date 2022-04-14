37069e5ca8f44db7a414b1397727240332c61a05
shanzj <gvssimux@qq.com>
2022-04-14 15:08:38 +0800

    删除forms页面重复id

---
+++ web/assets/js/shanzj/forms.js
@@ -0,0 +1,36 @@
+const vm = new Vue({
+    data() {
+        return {
+            pickerOptions: {
+                shortcuts: [{
+                    text: '今天',
+                    onClick(picker) {
+                        picker.$emit('pick', new Date());
+                    }
+                }, {
+                    text: '昨天',
+                    onClick(picker) {
+                        const date = new Date();
+                        date.setTime(date.getTime() - 3600 * 1000 * 24);
+                        picker.$emit('pick', date);
+                    }
+                }, {
+                    text: '一周前',
+                    onClick(picker) {
+                        const date = new Date();
+                        date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                        picker.$emit('pick', date);
+                    }
+                }]
+            },
+            value1: '',
+            value2: '',
+            value3: ''
+        };
+    },
+    method:{
+
+    }
+});
+
+vm.$mount("#root");
